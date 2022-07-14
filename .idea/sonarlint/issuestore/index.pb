6575d64a324f195a5341a54d4e67b6dd299fac01
Kacper <kacpergierycz@gmail.com>
2022-07-14 18:51:09 +0200

    stream kata's reduce

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+T
+$src/main/java/streams/UpperCase.java,8\9\8973637faf8721fc93d4163b54f990bd125f4172
+X
+(src/test/java/streams/UpperCaseTest.java,a\d\adcca331312ccbbe05f3fd92cf67ebf480ae984f
+V
+&src/test/java/streams/FlatMapTest.java,0\8\0865ee2893be74e539cbe53b4891db1358c1b211
+Z
+*src/test/java/streams/LetterCountTest.java,0\2\02b622d39806773f601ad74f79252eabfedef8d1
---
+++ src/main/java/streams/MaxAndComparator.java
@@ -0,0 +1,23 @@
+package streams;
+
+import java.util.*;
+
+public class MaxAndComparator {
+// }
+
+
+    public static Person getOldestPerson(List<Person> people) {
+        return people.stream().max(Comparator.comparingInt(Person::getAge)).get();
+    }
+
+    public static Person getOldestPerson2(List<Person> people) {
+        Person oldestPerson = new Person("", 0);
+        for (Person person : people) {
+            if (person.getAge() > oldestPerson.getAge()) {
+                oldestPerson = person;
+            }
+        }
+        return oldestPerson;
+    }
+
+}
